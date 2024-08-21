8fcf414097bbfe668b1b228ae723a5821a66b06a
dimas <dimasixc22@gmail.com>
2024-08-21 08:01:08 +0700

    middleware

--- cmd/echo/main.go
+++ cmd/echo/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"framework/config"
+	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -10,11 +11,31 @@ import (
 func main(){
 	router := echo.New()
 
-	router.GET("/", func(c echo.Context) error {
+	router.Use(Logger)
+
+	router.GET("/hello", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, echo.Map{
 			"message" : "hello world",
 		})
 	})
 
+	userRouter := router.Group("user")
+	{
+		userRouter.GET("", func(c echo.Context) error {
+			log.Println("log success from user")
+
+			return c.JSON(http.StatusOK, echo.Map{
+				"messsages" : "get from user",
+			})
+		})
+	}
+
 	router.Start(config.APP_PORT)
+}
+
+func Logger(next echo.HandlerFunc)echo.HandlerFunc{
+	return func(c echo.Context) error {
+		log.Println(c.Request().Method, c.Request().URL.Path)
+		return next(c)
+	}
 }
